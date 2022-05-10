d16db74de0b18a208ffed81f2db999ed35116668
dvir ben shlush <dvirbsh95@gmail.com>
2022-05-10 18:46:01 +0300

    add fields to todo-dialog and remove mock-data

--- src/app/components/new-todo/new-todo.component.ts
+++ src/app/components/new-todo/new-todo.component.ts
@@ -1,22 +1,38 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Form } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { ITodo } from 'src/app/models/todo.interface';
+import { TodoService } from 'src/app/services/todo.service';
+import {v4 as uuidv4 } from 'uuid'
 
 @Component({
   selector: 'app-new-todo',
   templateUrl: './new-todo.component.html',
-  styleUrls: ['./new-todo.component.css']
+  styleUrls: ['./new-todo.component.scss']
 })
 export class NewTodoComponent implements OnInit {
-  @ViewChild('f') form: Form;
+  @ViewChild('f') form: NgForm;
 
-  constructor() { }
+  constructor(public dialog:MatDialog, public todoService: TodoService) { }
 
   ngOnInit(): void {
   }
 
   public onNewTodoSubmit(): void{
-    console.log('on submit')
+    const formValues = this.form.form.value;
+
+    const newTodo: ITodo = {
+      id: uuidv4(),
+      title: formValues.title,
+      descripsion: formValues.descripsion,
+      isCompleted: formValues.isCompleted,
+      isArchived: formValues.isArchived,
+      endDate: formValues.endDate,
+      selected: formValues.selected
+    }
     console.log(this.form)
+    this.todoService.addNewTodo(newTodo); 
+    this.dialog.closeAll();
   }
 
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -6,132 +6,132 @@ import { ITodo } from '../models/todo.interface';
   providedIn: 'root'
 })
 export class TodoService {
-private mock =  [
-  {
-    "id" : 1,
-    "title": "Caracara, yellow-headed",
-    "descripsion": "Laceration without foreign body of abdominal wall, left upper quadrant with penetration into peritoneal cavity",
-    "isCompleted": false,
-    "isArchived": false,
-    "endDate": "5/28/2021",
-    "selected": true
-  },
-   {
-    "id" : 2,
-    "title": "Cat, civet",
-    "descripsion": "Other physeal fracture of unspecified metatarsal, initial encounter for open fracture",
-    "isCompleted": false,
-    "isArchived": false,
-    "endDate": "8/6/2021",
-    "selected": false
-  }, {
-    "id" : 3,
-    "title": "Caiman, spectacled",
-    "descripsion": "Nondisplaced fracture of base of third metacarpal bone, left hand, subsequent encounter for fracture with nonunion",
-    "isCompleted": false,
-    "isArchived": false,
-    "endDate": "9/6/2021",
-    "selected": false
-  }, {
-    "id" : 4,
-    "title": "Cat, tiger",
-    "descripsion": "Superficial foreign body of breast, unspecified breast, sequela",
-    "isCompleted": false,
-    "isArchived": false,
-    "endDate": "9/19/2021",
-    "selected": false
-  }, {
-    "id" : 5,
-    "title": "Dove, emerald-spotted wood",
-    "descripsion": "Accidental discharge from unspecified firearms or gun",
-    "isCompleted": false,
-    "isArchived": false,
-    "endDate": "10/6/2021",
-    "selected": false
-  }, {
-    "id" : 6,
-    "title": "White-rumped vulture",
-    "descripsion": "Anatomical narrow angle, left eye",
-    "isCompleted": false,
-    "isArchived": false,
-    "endDate": "2/28/2022",
-    "selected": false
-  }, {
-    "id" : 7,
-    "title": "Lion, steller's sea",
-    "descripsion": "Person on outside of car injured in collision with sport utility vehicle in traffic accident, initial encounter",
-    "isCompleted": false,
-    "isArchived": false,
-    "endDate": "9/13/2021",
-    "selected": false
-  }, {
-    "id" : 8,
-    "title": "Zebra, common",
-    "descripsion": "Poisoning by antirheumatics, not elsewhere classified, intentional self-harm, subsequent encounter",
-    "isCompleted": false,
-    "isArchived": false,
-    "endDate": "2/27/2022",
-    "selected": false
-  }, {
-    "id" : 9,
-    "title": "Kafue flats lechwe",
-    "descripsion": "Displaced segmental fracture of shaft of radius, left arm, subsequent encounter for closed fracture with malunion",
-    "isCompleted": false,
-    "isArchived": false,
-    "endDate": "12/23/2021",
-    "selected": false
-  }, {
-    "id" : 10,
-    "title": "Francolin, coqui",
-    "descripsion": "Frostbite with tissue necrosis of right ankle, sequela",
-    "isCompleted": false,
-    "isArchived": false,
-    "endDate": "4/1/2021",
-    "selected": false
-  }, {
-    "id" : 11,
-    "title": "Crested bunting",
-    "descripsion": "Type AB blood, Rh positive",
-    "isCompleted": false,
-    "isArchived": true,
-    "endDate": "3/16/2022",
-    "selected": false
-  }, {
-    "id" : 12,
-    "title": "Indian star tortoise",
-    "descripsion": "Other optic atrophy, unspecified eye",
-    "isCompleted": false,
-    "isArchived": true,
-    "endDate": "8/7/2021",
-    "selected": false
-  }, {
-    "id" : 13,
-    "title": "Wolf spider",
-    "descripsion": "Adverse effect of unspecified systemic anti-infective and antiparasitic, initial encounter",
-    "isCompleted": false,
-    "isArchived": false,
-    "endDate": "5/22/2021",
-    "selected": false
-  }, {
-    "id" : 14,
-    "title": "Common wombat",
-    "descripsion": "Maternal care for hydrops fetalis, unspecified trimester",
-    "isCompleted": false,
-    "isArchived": false,
-    "endDate": "5/19/2021",
-    "selected": false
-  }, {
-    "id" : 15,
-    "title": "Plover, three-banded",
-    "descripsion": "Loose body in knee",
-    "isCompleted": false,
-    "isArchived": false,
-    "endDate": "12/12/2021",
-    "selected": false
-  }];
-
-  private _todoSubject: BehaviorSubject<Array<ITodo>> = new BehaviorSubject(this.mock);
-  private _singleTodoSubject: Subject<ITodo> = new BehaviorSubject(this.mock[0]);
+// private mock =  [
+//   {
+//     "id" : 1,
+//     "title": "Caracara, yellow-headed",
+//     "descripsion": "Laceration without foreign body of abdominal wall, left upper quadrant with penetration into peritoneal cavity",
+//     "isCompleted": false,
+//     "isArchived": false,
+//     "endDate": "5/28/2021",
+//     "selected": true
+//   },
+//    {
+//     "id" : 2,
+//     "title": "Cat, civet",
+//     "descripsion": "Other physeal fracture of unspecified metatarsal, initial encounter for open fracture",
+//     "isCompleted": false,
+//     "isArchived": false,
+//     "endDate": "8/6/2021",
+//     "selected": false
+//   }, {
+//     "id" : 3,
+//     "title": "Caiman, spectacled",
+//     "descripsion": "Nondisplaced fracture of base of third metacarpal bone, left hand, subsequent encounter for fracture with nonunion",
+//     "isCompleted": false,
+//     "isArchived": false,
+//     "endDate": "9/6/2021",
+//     "selected": false
+//   }, {
+//     "id" : 4,
+//     "title": "Cat, tiger",
+//     "descripsion": "Superficial foreign body of breast, unspecified breast, sequela",
+//     "isCompleted": false,
+//     "isArchived": false,
+//     "endDate": "9/19/2021",
+//     "selected": false
+//   }, {
+//     "id" : 5,
+//     "title": "Dove, emerald-spotted wood",
+//     "descripsion": "Accidental discharge from unspecified firearms or gun",
+//     "isCompleted": false,
+//     "isArchived": false,
+//     "endDate": "10/6/2021",
+//     "selected": false
+//   }, {
+//     "id" : 6,
+//     "title": "White-rumped vulture",
+//     "descripsion": "Anatomical narrow angle, left eye",
+//     "isCompleted": false,
+//     "isArchived": false,
+//     "endDate": "2/28/2022",
+//     "selected": false
+//   }, {
+//     "id" : 7,
+//     "title": "Lion, steller's sea",
+//     "descripsion": "Person on outside of car injured in collision with sport utility vehicle in traffic accident, initial encounter",
+//     "isCompleted": false,
+//     "isArchived": false,
+//     "endDate": "9/13/2021",
+//     "selected": false
+//   }, {
+//     "id" : 8,
+//     "title": "Zebra, common",
+//     "descripsion": "Poisoning by antirheumatics, not elsewhere classified, intentional self-harm, subsequent encounter",
+//     "isCompleted": false,
+//     "isArchived": false,
+//     "endDate": "2/27/2022",
+//     "selected": false
+//   }, {
+//     "id" : 9,
+//     "title": "Kafue flats lechwe",
+//     "descripsion": "Displaced segmental fracture of shaft of radius, left arm, subsequent encounter for closed fracture with malunion",
+//     "isCompleted": false,
+//     "isArchived": false,
+//     "endDate": "12/23/2021",
+//     "selected": false
+//   }, {
+//     "id" : 10,
+//     "title": "Francolin, coqui",
+//     "descripsion": "Frostbite with tissue necrosis of right ankle, sequela",
+//     "isCompleted": false,
+//     "isArchived": false,
+//     "endDate": "4/1/2021",
+//     "selected": false
+//   }, {
+//     "id" : 11,
+//     "title": "Crested bunting",
+//     "descripsion": "Type AB blood, Rh positive",
+//     "isCompleted": false,
+//     "isArchived": true,
+//     "endDate": "3/16/2022",
+//     "selected": false
+//   }, {
+//     "id" : 12,
+//     "title": "Indian star tortoise",
+//     "descripsion": "Other optic atrophy, unspecified eye",
+//     "isCompleted": false,
+//     "isArchived": true,
+//     "endDate": "8/7/2021",
+//     "selected": false
+//   }, {
+//     "id" : 13,
+//     "title": "Wolf spider",
+//     "descripsion": "Adverse effect of unspecified systemic anti-infective and antiparasitic, initial encounter",
+//     "isCompleted": false,
+//     "isArchived": false,
+//     "endDate": "5/22/2021",
+//     "selected": false
+//   }, {
+//     "id" : 14,
+//     "title": "Common wombat",
+//     "descripsion": "Maternal care for hydrops fetalis, unspecified trimester",
+//     "isCompleted": false,
+//     "isArchived": false,
+//     "endDate": "5/19/2021",
+//     "selected": false
+//   }, {
+//     "id" : 15,
+//     "title": "Plover, three-banded",
+//     "descripsion": "Loose body in knee",
+//     "isCompleted": false,
+//     "isArchived": false,
+//     "endDate": "12/12/2021",
+//     "selected": false
+//   }];
+  private todos: ITodo[] = [];
+  private _todoSubject: BehaviorSubject<Array<ITodo>> = new BehaviorSubject(this.todos);
+  private _singleTodoSubject: Subject<ITodo> = new BehaviorSubject(this.todos.length? this.todos[0] : null);
 
   constructor() { }
 
@@ -146,5 +146,13 @@ private mock =  [
 
   public setSelectedTodo(todo: ITodo){
     return this._singleTodoSubject.next(todo);
+  }
+
+  public addNewTodo(newTodo: ITodo): void{
+    console.log('id ' + JSON.stringify(newTodo))
+
+    const exitistingTodo: Array<ITodo> = this._todoSubject.value;
+    exitistingTodo.push(newTodo);
+    this._todoSubject.next(exitistingTodo);
   }
 }
